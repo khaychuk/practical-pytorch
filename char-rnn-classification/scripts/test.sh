34552d81ce041bb4dde28903a8083c91ad14797f
DanielTakeshi <takeshidanny@gmail.com>
2018-05-04 23:22:26 -0700

    adding a script to run overnight, for now just testing hidden layer dimension

---
+++ char-rnn-classification/scripts/test.sh
@@ -0,0 +1,12 @@
+
+python train.py --seed 0 --n_hidden 16 --batch_size 32 --optimizer adam --learning_rate 0.001  
+python train.py --seed 1 --n_hidden 16 --batch_size 32 --optimizer adam --learning_rate 0.001
+python train.py --seed 2 --n_hidden 16 --batch_size 32 --optimizer adam --learning_rate 0.001
+
+python train.py --seed 0 --n_hidden 128 --batch_size 32 --optimizer adam --learning_rate 0.001  
+python train.py --seed 1 --n_hidden 128 --batch_size 32 --optimizer adam --learning_rate 0.001
+python train.py --seed 2 --n_hidden 128 --batch_size 32 --optimizer adam --learning_rate 0.001
+
+python train.py --seed 0 --n_hidden 1024 --batch_size 32 --optimizer adam --learning_rate 0.001  
+python train.py --seed 1 --n_hidden 1024 --batch_size 32 --optimizer adam --learning_rate 0.001
+python train.py --seed 2 --n_hidden 1024 --batch_size 32 --optimizer adam --learning_rate 0.001
